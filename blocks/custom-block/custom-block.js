acd3c5ce0faaff86780ad2c6bf103cc99c95cd86
priya-3109 <priyanka.shingne@deptagency.com>
2026-08-06 17:41:38 +0530

    adding styling

--- blocks/custom-block/custom-block.js
+++ blocks/custom-block/custom-block.js
@@ -1,14 +1,9 @@
 export default function decorate(block) {
-[...block.children].forEach((card) => {
-  console.log(card);
-const cardWrapper = card.closest('.custom-block-node');
-cardWrapper.querySelectorAll('div').forEach((item) => {
-    item.classList.add('cards-item');    
-   if(item.classList.contains('cards-item')){
-    item.querySelectorAll('.cards-item picture img').forEach((img) => {
-        img.classList.add('cards-item-img');
+  [...block.children].forEach((item) => {
+    item.classList.add('cards-item');
+
+    item.querySelectorAll('picture img').forEach((img) => {
+      img.classList.add('cards-item-img');
     });
-  }
-});
-});
+  });
 }
