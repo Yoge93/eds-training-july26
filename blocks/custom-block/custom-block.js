84545bfbbc024b071f69a8276fa80b56b24d471c
priya-3109 <priyanka.shingne@deptagency.com>
2026-08-06 12:58:57 +0530

    custom classes added

--- blocks/custom-block/custom-block.js
+++ blocks/custom-block/custom-block.js
@@ -1,5 +1,9 @@
 export default function decorate(block) {
 [...block.children].forEach((card) => {
   console.log(block);
+  card.classList.add('custom-block-container');
+  card.querySelectorAll('[data-aue-model="custom-block-item"]').forEach((div) => {
+    div.classList.add('custom-cards');
+  });
 });
 }
