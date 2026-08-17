5cf1a38bfd395defc03819506789134df85da9d0
priya-3109 <priyanka.shingne@deptagency.com>
2026-08-17 17:18:20 +0530

    decorate function added

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,19 +1,5 @@
 export default function decorate(block) {
   const items = [...block.children];
 
-  items.forEach((item) => {
-    const rows = [...item.children];
-
-    const title = rows[0];
-    const content = rows[1];
-
-    if (!title || !content) return;
-
-    title.classList.add('accordion-title');
-    content.classList.add('accordion-content');
-
-    title.addEventListener('click', function () {
-      item.classList.toggle('open');
-    });
-  });
+  console.log("items", items);
 }
